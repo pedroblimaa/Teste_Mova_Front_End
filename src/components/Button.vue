ebaced154e917b1bd3183b3d09aa87671a927afa
Pedro Bonfilio <pedroblimaa@gmail.com>
2021-04-23 11:02:32 -0300

    Subindo Projeto

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button v-on:click="$emit('clicked')" class="btn">{{txt}}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+    props: ['txt']
+};
+</script>
+
+<style scoped>
+.btn {
+    transition: 0.2s;
+    cursor: pointer;
+    padding: 10px 20px;
+    border: 1px solid grey;
+    border-radius: 3px;
+    background-color: #1f1f1f;
+    color: #fff;
+}
+
+.btn:hover {
+    cursor: pointer;
+    background-color: #111111
+}
+
+</style>
